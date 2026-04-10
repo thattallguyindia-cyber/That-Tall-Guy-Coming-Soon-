247ab32f54f5d0ee9f52c63e113a80f3086a63c3
Vidit0199 <Viditrajarchitects@gmail.com>
2026-04-10 16:54:54 +0530

    fix sitemap format

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'http://localhost:3000', // for now (change later to real domain)
+  siteUrl:'https://thattallguyindia.com', // for now (change later to real domain)
   generateRobotsTxt: true,
 }
